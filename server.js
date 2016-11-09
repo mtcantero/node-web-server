3035f374ae28317ca78b6745438509e884d8de35
mtcantero <mario.trueba@outlook.com>
2016-11-09 21:53:38 +0000

    First Commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('viewengine','hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt' ,(text) => {
+    return text.toUpperCase();
+})
+
+app.get('/', (req,res) => {
+    res.render('home.hbs',{
+        pageTitle:'Home',
+        
+        welcomeMessage: 'Hello everyone!!'
+    });
+
+});
+
+app.get('/about', (req,res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+        
+    });
+});
+
+app.get('/bad', (req,res) => {
+    res.send({
+        errormessage: 'Bad Request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
